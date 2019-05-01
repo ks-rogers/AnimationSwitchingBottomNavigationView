bb761ee841f4f1898554e476ef5603c06f87fc7f
kettsun0123 <kyuji7123.development@gmail.com>
2019-05-01 22:20:06 +0900

    first reefactor

--- library/src/main/kotlin/jp/co/ksrogers/animationswitchingbottomnavigation/AnimationSwitchingBottomNavigationLayout.kt
+++ library/src/main/kotlin/jp/co/ksrogers/animationswitchingbottomnavigation/AnimationSwitchingBottomNavigationLayout.kt
@@ -5,10 +5,11 @@ import android.animation.AnimatorSet
 import android.content.Context
 import android.content.res.ColorStateList
 import android.util.AttributeSet
-import android.util.Log
 import android.view.Gravity
 import android.widget.FrameLayout
 import androidx.annotation.NonNull
+import jp.co.ksrogers.animationswitchingbottomnavigation.AnimationSwitchingBottomNavigationLayout.SelectedButtonSize.NORMAL
+import jp.co.ksrogers.animationswitchingbottomnavigation.AnimationSwitchingBottomNavigationLayout.SelectedButtonSize.SMALL
 import jp.co.ksrogers.animationswitchingbottomnavigation.ext.animatorAlpha
 import jp.co.ksrogers.animationswitchingbottomnavigation.ext.animatorX
 import jp.co.ksrogers.animationswitchingbottomnavigation.ext.playSequentiallyExt
@@ -28,6 +29,19 @@ class AnimationSwitchingBottomNavigationLayout @JvmOverloads constructor(
   context: Context,
   attrs: AttributeSet? = null
 ) : FrameLayout(context, attrs) {
+
+  private enum class SelectedButtonSize {
+    NORMAL,
+    SMALL;
+
+    companion object {
+      fun fromIndex(index: Int): SelectedButtonSize = when (index) {
+        0 -> NORMAL
+        1 -> SMALL
+        else -> throw IllegalArgumentException("Unexpected attribute: selectedButtonSize=$index")
+      }
+    }
+  }
 
   companion object {
     const val NAVIGATION_VIEW_INDEX = 1
@@ -101,6 +115,31 @@ class AnimationSwitchingBottomNavigationLayout @JvmOverloads constructor(
       )
       buttonBackgroundColor =
         a.getColorStateList(R.styleable.AnimationSwitchingBottomNavigationLayout_buttonBackgroundColor)
+      navigationViewHeight =
+        a.getDimensionPixelSize(
+          R.styleable.AnimationSwitchingBottomNavigationLayout_navigationViewHeight,
+          resources.getDimensionPixelSize(R.dimen.animation_switching_bottom_navigation_default_height)
+        )
+      val selectedButtonSize = SelectedButtonSize.fromIndex(
+        a.getInt(
+          R.styleable.AnimationSwitchingBottomNavigationLayout_selectedButtonSize,
+          0
+        )
+      )
+      when (selectedButtonSize) {
+        NORMAL -> {
+          selectedWidth =
+            resources.getDimensionPixelSize(R.dimen.animation_switching_bottom_navigation_selected_default_width)
+          selectedHeight =
+            resources.getDimensionPixelSize(R.dimen.animation_switching_bottom_navigation_selected_default_height)
+        }
+        SMALL -> {
+          selectedWidth =
+            resources.getDimensionPixelSize(R.dimen.animation_switching_bottom_navigation_selected_small_width)
+          selectedHeight =
+            resources.getDimensionPixelSize(R.dimen.animation_switching_bottom_navigation_selected_small_height)
+        }
+      }
       a.recycle()
     }
 
@@ -123,53 +162,47 @@ class AnimationSwitchingBottomNavigationLayout @JvmOverloads constructor(
   }
 
   override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    val layoutWidthMode = MeasureSpec.getMode(widthMeasureSpec)
+    val layoutWidth = MeasureSpec.getSize(widthMeasureSpec)
+    val layoutHeightMode = MeasureSpec.getMode(heightMeasureSpec)
+    val layoutHeight = MeasureSpec.getSize(heightMeasureSpec)
+
+    // modeがEXACTLYであるかチェック
+    if (layoutWidthMode != MeasureSpec.EXACTLY || layoutHeightMode != MeasureSpec.EXACTLY) {
+      throw IllegalStateException("Please set 'match_parent' to AnimationSwitchingBottomNavigationLayout's width and height.")
+    }
+
     // NavigationViewのサイズを確定
-    val navigationWidth = navigationView.measuredWidth
-    val navigationHeight = navigationView.measuredHeight
-    if (navigationWidth != this.measuredWidth) {
-      Log.d(
-        TAG_NAME,
-        "Nav width is not same as this width. nav=($navigationWidth, $navigationHeight) and this=($measuredWidth, $measuredHeight)"
-      )
-    }
-    if (navigationHeight > navigationViewHeight) {
-      val navigationWidthSpec = MeasureSpec.makeMeasureSpec(navigationWidth, MeasureSpec.AT_MOST)
-      val navigationHeightSpec =
-        MeasureSpec.makeMeasureSpec(navigationViewHeight, MeasureSpec.EXACTLY)
-      navigationView.measure(navigationWidthSpec, navigationHeightSpec)
-    }
+    val navigationWidth = layoutWidth
+    val navigationWidthSpec = MeasureSpec.makeMeasureSpec(navigationWidth, MeasureSpec.AT_MOST)
+    val navigationHeightSpec =
+      MeasureSpec.makeMeasureSpec(navigationViewHeight, MeasureSpec.EXACTLY)
+    navigationView.measure(navigationWidthSpec, navigationHeightSpec)
+
     // SelectedButtonのサイズを確定
     val selectedWidthSpec = MeasureSpec.makeMeasureSpec(selectedWidth, MeasureSpec.EXACTLY)
     val selectedHeightSpec = MeasureSpec.makeMeasureSpec(selectedHeight, MeasureSpec.EXACTLY)
     selectedButton.measure(selectedWidthSpec, selectedHeightSpec)
+
     // メインコンテンツのサイズを確定
     val content = getChildAt(0)
-    val contentWidthSpec = MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY)
+    val contentWidthSpec = MeasureSpec.makeMeasureSpec(layoutWidth, MeasureSpec.EXACTLY)
     val contentHeightSpec = MeasureSpec.makeMeasureSpec(
-      measuredHeight - navigationView.measuredHeight,
+      layoutHeight - navigationViewHeight,
       MeasureSpec.EXACTLY
     )
     content.measure(contentWidthSpec, contentHeightSpec)
-    Log.d(
-      TAG_NAME,
-      "NavView(width=${navigationView.measuredWidth}, height=${navigationView.measuredHeight})"
-    )
-    Log.d(TAG_NAME, "NavLayout(width=$measuredWidth, height=$measuredHeight)")
+
+    setMeasuredDimension(layoutWidth, layoutHeight)
   }
 
   override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-    // superを呼ぶとすでに子ビューにはparamsが設定されている
     super.onLayout(changed, left, top, right, bottom)
     // SelectedButtonの位置を確定
     val selectedWidth = selectedButton.measuredWidth
     val selectedHeight = selectedButton.measuredHeight
     val itemViewWidth = navigationView.menuView.itemViews[0].measuredWidth
     val differenceBetweeensselectedAndItem = (itemViewWidth - selectedWidth) / 2
-    Log.d(
-      TAG_NAME,
-      "selected(left=${selectedButton.left}, top=${selectedButton.top}, right=${selectedButton.right}, bottom=${selectedButton.bottom})"
-    )
     selectedButton.layout(
       differenceBetweeensselectedAndItem,
       measuredHeight - selectedHeight - selectedBottomMargin,
